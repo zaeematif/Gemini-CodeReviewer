ebce8bcac0dda02c750d1104e27902c4dbb8c744
Zaeem Atif <67412599+zaeematif@users.noreply.github.com>
2025-02-23 20:35:29 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,10 +17,10 @@ app.use(cors({
 }));
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
   })
 }
 
